86ae2ca5a646cb8ca77fa68eed73c6397ec1ade4
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-14 21:59:26 +0100

    Initial commit, create basic layout, fill with starting personal informaiton

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Gary Gillespie",
+    "role": "Graduate Software Engineer / Freelance Developer",
+    "linkedinId":"gary-gillespie-469a14174",
+    "skypeid": "gzgillespie@outlook.com",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/gary-gillespie-469a14174/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/ggillespie948",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Scotland",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Dundee",
+        "specialization":"Bachelor of Science - BS, Applied Computing, First Class Honours",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2018",
+        "Achievements":"2017/2018 Open Award Winner, Venture 2018 Finalist, Technology Innovation Management 2017 Prize Winner, 2015 NCR Prize"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"BannTech Limited",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2019",
+        "Achievements":"Worked to create bespoke IMS/CMS web applications. Along with the creation of data-driven information management systems also worked on several content management systems and front facing websites requiring website design, integration of various APIs",
+        "skills" :[],
+      },
+      {
+        "CompanyName":"Freelance Developer",
+        "specialization":"Web development, Games development",
+        "MonthOfStarting":"May",
+        "YearOfStarting":"2017",
+        "MonthOfLeaving":"Present",
+        "YearOfLeaving":"",
+        "Achievements":"Worked to create bespoke IMS/CMS web applications. Along with the creation of data-driven information management systems also worked on several content management systems and front facing websites requiring website design, integration of various APIs",
+        "skills" :[],
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"C#"
+      },
+      {
+        "skillname":".Net Core"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
