db4932f897bfec2d23bf8a3476944e1e2b1135fc
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-05-15 01:04:34 +0100

    Updates

---
+++ precache-manifest.3be5142c3456b8a9b3a622e631451554.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f233af6cdff275881780",
+    "url": "/ggillespie948/portfolio-spa-react/static/js/runtime~main.f233af6c.js"
+  },
+  {
+    "revision": "e6e4b6b9df867580c9e2",
+    "url": "/ggillespie948/portfolio-spa-react/static/js/main.e6e4b6b9.chunk.js"
+  },
+  {
+    "revision": "7c1661cd19304614b361",
+    "url": "/ggillespie948/portfolio-spa-react/static/js/1.7c1661cd.chunk.js"
+  },
+  {
+    "revision": "e6e4b6b9df867580c9e2",
+    "url": "/ggillespie948/portfolio-spa-react/static/css/main.1f8e2388.chunk.css"
+  },
+  {
+    "revision": "d3e3093a9ec3d8f38b1620fb6a47ac88",
+    "url": "/ggillespie948/portfolio-spa-react/index.html"
+  }
+];
